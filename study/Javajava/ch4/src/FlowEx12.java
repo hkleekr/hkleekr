cb72e90dd6eac59aa3eeb8c71347ce232de893c1
Hyekyung <mwjer333@gmail.com>
2022-09-22 00:34:24 +0900

    자바의 정석 ch4. 조건문과 반복문 예제

---
+++ study/Javajava/ch4/src/FlowEx12.java
@@ -0,0 +1,9 @@
+public class FlowEx12 {
+    public static void main(String[] args) {
+        for (int i = 1; i<=5;i++)
+            System.out.println(i);  // i 값을 출력
+        for (int i = 1; i<=5; i++)
+            System.out.print(i);
+        System.out.println();
+    }
+}
